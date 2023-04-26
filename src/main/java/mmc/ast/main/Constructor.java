1bc30cb57bb655c9520bfc567eff297901fd6f03
i21023 <i21023@hb.dhbw-stuttgart.de>
2023-04-26 13:30:22 +0200

    stream() entfernt

---
+++ src/main/java/mmc/ast/main/Constructor.java
@@ -0,0 +1,15 @@
+package mmc.ast.main;
+
+import mmc.ast.statements.Statement;
+import org.objectweb.asm.ClassWriter;
+
+import java.util.List;
+
+public class Constructor {
+    List<Parameter> parameters;
+    Statement statement;
+
+    public void codeGen(ClassWriter cw){
+
+    }
+}
