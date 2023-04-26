d75826eda9cbcf4084508b215646092cf289718b
nxxnnaa <i21022@hb.dhbw-stuttgart.de>
2023-04-26 10:41:35 +0200

    Grammatik File erstellt

---
+++ src/main/parser/ANTLR/ParserListener.java
@@ -0,0 +1,29 @@
+// Generated from C:/Users/nina-/Documents/Programmieren/viertes-Semester/src/main/parser\Parser.g4 by ANTLR 4.12.0
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link ParserParser}.
+ */
+public interface ParserListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link ParserParser#s}.
+	 * @param ctx the parse tree
+	 */
+	void enterS(ParserParser.SContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserParser#s}.
+	 * @param ctx the parse tree
+	 */
+	void exitS(ParserParser.SContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ParserParser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void enterExpr(ParserParser.ExprContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ParserParser#expr}.
+	 * @param ctx the parse tree
+	 */
+	void exitExpr(ParserParser.ExprContext ctx);
+}
