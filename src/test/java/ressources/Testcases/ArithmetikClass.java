3872171948dddba7420ab767d0090c1e0ef267ae
Micha Hoelle <106805499+Yosgarat@users.noreply.github.com>
2023-05-03 10:33:58 +0200

    Some more Classes added

---
+++ src/test/java/ressources/Testcases/ArithmetikClass.java
@@ -0,0 +1,27 @@
+package ressources.testcases;
+
+public class ArithmetikClass {
+
+    public int add(int a, int b){
+        if(b==0)
+        {
+            return a;
+        }
+        return add(a+1,b-1);
+    }
+
+    public int mul(int a, int b)
+    {
+        if(b==0)
+        {
+            return 0;
+        }
+        if(b==1)
+        {
+            return a;
+        }
+        return add(a, mul(a,b-1));
+
+    }
+
+}
