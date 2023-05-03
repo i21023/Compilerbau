9b789e7f6cf440181fe1a15b2fd7b73c3919c65b
emmsie2812 <emma.weiss2000@gmail.com>
2023-05-03 11:07:45 +0200

    AccessModifier eingefügt

--- src/main/java/mmc/ast/main/Constructor.java
+++ src/main/java/mmc/ast/main/Constructor.java
@@ -1,6 +1,8 @@
 package mmc.ast.main;
 
 import mmc.Visitable;
+import mmc.ast.AccessModifier;
+import mmc.ast.statements.IStatement;
 import mmc.semantikcheck.SemanticVisitor;
 import mmc.semantikcheck.TypeCheckResult;
 import org.objectweb.asm.ClassWriter;
@@ -9,12 +11,14 @@ import java.util.List;
 
 public class Constructor implements Visitable {
     public List<Parameter> parameters;
-    public Statement statement;
+    public IStatement statement;
+    public AccessModifier accessModifier;
 
-    public Constructor(Statement pStatement, List<Parameter> pParameters)
+    public Constructor(IStatement pStatement, List<Parameter> pParameters, AccessModifier pAccessModifier)
     {
         statement=pStatement;
         parameters=pParameters;
+        accessModifier=pAccessModifier;
     }
 
     public void codeGen(ClassWriter cw){
--- src/main/java/mmc/ast/main/Method.java
+++ src/main/java/mmc/ast/main/Method.java
@@ -1,7 +1,9 @@
 package mmc.ast.main;
 
 import mmc.Visitable;
+import mmc.ast.AccessModifier;
 import mmc.ast.Type;
+import mmc.ast.statements.IStatement;
 import mmc.semantikcheck.SemanticVisitor;
 import mmc.semantikcheck.TypeCheckResult;
 import org.objectweb.asm.ClassWriter;
@@ -12,14 +14,16 @@ public class Method implements Visitable {
     public Type type;
     public String name;
     public List<Parameter> parameters;
-    public Statement statement;
+    public IStatement statement;
+    public AccessModifier accessModifier;
 
-    public Method(Type pType, String pName, List<Parameter> pParameters, Statement pStatement)
+    public Method(Type pType, String pName, List<Parameter> pParameters, IStatement pStatement, AccessModifier pAccessModifier)
     {
         type=pType;
         name=pName;
         parameters=pParameters;
         statement=pStatement;
+        accessModifier=pAccessModifier;
     }
 
     public void codeGen(ClassWriter cw) {
