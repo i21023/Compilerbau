9f084993607203576ddb594fe8db322e65f9d316
i21023 <i21023@hb.dhbw-stuttgart.de>
2023-05-17 11:42:12 +0200

    field var values getting set in constructors

--- src/main/java/mmc/codegen/visitors/ClassCodeGenerator.java
+++ src/main/java/mmc/codegen/visitors/ClassCodeGenerator.java
@@ -1,11 +1,12 @@
 package mmc.codegen.visitors;
 
-import mmc.ast.BasicType;
-import mmc.ast.ReferenceType;
-import mmc.ast.Type;
+import mmc.ast.*;
+import mmc.ast.expressions.LocalOrFieldVar;
 import mmc.ast.main.ClassDecl;
 import mmc.ast.main.Constructor;
 import mmc.ast.main.Field;
+import mmc.ast.statementexpression.Assign;
+import mmc.ast.statements.Block;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.Opcodes;
@@ -36,12 +37,34 @@ public class ClassCodeGenerator implements IClassCodeVisitor{
         //Generate Field Bytecode
         classDecl.fields.forEach(field -> field.accept(this));
 
+        classDecl.fields.stream().filter(field -> field.expression != null).forEach(field -> {
+            classDecl.constructors.forEach(constructor -> {
+                constructor.statement.statements.add(0, new Assign(new LocalOrFieldVar(field.name), field.expression, null));
+            });
+        });
+
         //Generate Constructor Bytecode
         if(classDecl.constructors.isEmpty()){
-            new Constructor().accept(new MethodCodeGenerator(classWriter, fieldVars, classDecl.name));
+            //generate default Constructor
+            Constructor defaultConstructor = new Constructor();
+
+            //add all fieldvar Exprs to default constructor
+            classDecl.fields.stream().filter(field -> field.expression != null).forEach(field -> {
+                    defaultConstructor.statement.statements.add(0, new Assign(new LocalOrFieldVar(field.name),
+                            field.expression, null));
+            });
+
+            defaultConstructor.accept(new MethodCodeGenerator(classWriter, fieldVars, classDecl.name));
+
         }
         else{
-            classDecl.constructors.forEach(constructor -> constructor.accept(new MethodCodeGenerator(classWriter, fieldVars, classDecl.name)));
+            classDecl.constructors.forEach(constructor -> {
+                classDecl.fields.stream().filter(field -> field.expression != null).forEach(field -> {
+                    constructor.statement.statements.add(0, new Assign(new LocalOrFieldVar(field.name),
+                            field.expression, null));
+                    constructor.accept(new MethodCodeGenerator(classWriter, fieldVars, classDecl.name));
+                });
+            });
         }
 
         //Generate Method ByteCode
@@ -51,6 +74,7 @@ public class ClassCodeGenerator implements IClassCodeVisitor{
 
     @Override
     public void visit(Field field) {
+
         fieldVars.put(field.name, field.type);
         FieldVisitor fieldVisitor = classWriter.visitField(GeneratorHelpFunctions.getAccessModifier(field.accessModifier, field.isStatic), field.name,
                 GeneratorHelpFunctions.getDescriptor(null, field.getType()), null, null);
