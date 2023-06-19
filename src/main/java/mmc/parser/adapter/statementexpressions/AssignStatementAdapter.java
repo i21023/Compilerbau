3910bace01d54c792f6268d35df69c9639b82521
Hannes Kollert <hako94@web.de>
2023-06-19 15:11:02 +0200

    Change Assign logic in Parser

--- src/main/java/mmc/parser/adapter/statementexpressions/AssignStatementAdapter.java
+++ src/main/java/mmc/parser/adapter/statementexpressions/AssignStatementAdapter.java
@@ -1,12 +1,11 @@
 package mmc.parser.adapter.statementexpressions;
 
 import mmc.ast.Operator;
-import mmc.ast.expressions.Binary;
 import mmc.ast.expressions.IExpression;
 import mmc.ast.expressions.LocalOrFieldVar;
 import mmc.ast.statementexpression.Assign;
-import mmc.parser.adapter.expressions.InstVarAdapter;
 import mmc.parser.adapter.expressions.ExpressionAdapter;
+import mmc.parser.adapter.expressions.InstVarAdapter;
 import mmc.parser.antlr.MiniJavaParser;
 
 public class AssignStatementAdapter {
@@ -23,39 +22,36 @@ public class AssignStatementAdapter {
 
         IExpression rightExpr = ExpressionAdapter.adapt(assignStatement.expr());
 
+        Operator assignOperator = null;
         switch (assignStatement.assign_op().getText()) {
             case "=":
+                assignOperator = Operator.ASSIGN;
                 break;
             case "+=":
-                rightExpr = new Binary(Operator.PLUS, leftExpr, rightExpr,
-                        assignStatement.getStart().getLine(),
-                        assignStatement.getStop().getLine());
+                //rightExpr = new Binary(Operator.PLUS, leftExpr, rightExpr, assignStatement.getStart().getLine(), assignStatement.getStop().getLine());
+                assignOperator = Operator.PLUSASSIGN;
                 break;
             case "-=":
-                rightExpr = new Binary(Operator.MINUS, leftExpr, rightExpr,
-                        assignStatement.getStart().getLine(),
-                        assignStatement.getStop().getLine());
+                //rightExpr = new Binary(Operator.MINUS, leftExpr, rightExpr, assignStatement.getStart().getLine(), assignStatement.getStop().getLine());
+                assignOperator = Operator.MINUSASSIGN;
                 break;
             case "*=":
-                rightExpr = new Binary(Operator.MULT, leftExpr, rightExpr,
-                        assignStatement.getStart().getLine(),
-                        assignStatement.getStop().getLine());
+                //rightExpr = new Binary(Operator.MULT, leftExpr, rightExpr, assignStatement.getStart().getLine(), assignStatement.getStop().getLine());
+                assignOperator = Operator.MULTASSIGN;
                 break;
             case "/=":
-                rightExpr = new Binary(Operator.DIV, leftExpr, rightExpr,
-                        assignStatement.getStart().getLine(),
-                        assignStatement.getStop().getLine());
+                //rightExpr = new Binary(Operator.DIV, leftExpr, rightExpr, assignStatement.getStart().getLine(), assignStatement.getStop().getLine());
+                assignOperator = Operator.DIVASSIGN;
                 break;
             case "%=":
-                rightExpr = new Binary(Operator.MOD, leftExpr, rightExpr,
-                        assignStatement.getStart().getLine(),
-                        assignStatement.getStop().getLine());
+                //rightExpr = new Binary(Operator.MOD, leftExpr, rightExpr, assignStatement.getStart().getLine(), assignStatement.getStop().getLine());
+                assignOperator = Operator.MODASSIGN;
                 break;
             default:
                 break;
         }
 
-        return new Assign(leftExpr, rightExpr,
+        return new Assign(assignOperator, leftExpr, rightExpr,
                 assignStatement.getStart().getLine(),
                 assignStatement.getStop().getLine());
     }
